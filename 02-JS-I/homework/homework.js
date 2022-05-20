2b9a92f0278a70f4c84cea60d9aee9f9be70e1f1
felipeanta <FelipeAnta>
2022-05-20 14:47:18 -0300

    7 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -188,8 +188,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = "Hola "
-  return saludo + nombre;
+  var saludo = "Hola"
+  return saludo + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
